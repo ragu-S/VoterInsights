76499eefc6c028e81ccf961f0b6f35ea2a0c2c42
Rohit Verma <rohitverma@live.ca>
2014-06-01 09:20:03 -0400

    Updating default router

--- www/js/app.js
+++ www/js/app.js
@@ -96,6 +96,6 @@ angular.module('starter', ['ionic', 'starter.controllers'])
                 }
             });
         // if none of the above states are matched, use this as the fallback
-//  $urlRouterProvider.otherwise('/app/question');
+  $urlRouterProvider.otherwise('/app/postalcode');
     });
 
